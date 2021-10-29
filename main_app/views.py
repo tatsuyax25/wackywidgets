9d274cf3f924adc30154b119c40a6de0f16e7367
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-10-29 14:01:04 -0700

    Fix bug

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('Wacky Widgets!')
+    return HttpResponse('<h1>Wacky Widgets!</h1>')
 
 
