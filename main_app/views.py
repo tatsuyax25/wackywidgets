f8f1483b1d2e3766b319488d3c591f27fb0c9ee0
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-10-29 15:03:04 -0700

    Migrate

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, DeleteView
+
+from main_app.models import Widget
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 
+class WidgetCreate(CreateView):
+    description = Widget
+    quantity = '__all__'
+
+class WidgetDelete(DeleteView):
+    description = Widget
+    quantity = '__all__'
