71296bafbb0050fe23c93413804f6f808b9a30f2
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-10-29 17:04:02 -0700

    Update

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('widgets/create/', views.add_widget, name='widgets_create'),
+    path('widgets/delete/<int:widget_id>', views.delete_widget, name='widgets_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, DeleteView
-
+from .form import WidgetForm
 from main_app.models import Widget
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    widget_form = WidgetForm(request.POST)
+    widgets = Widget.objects.all()
+    return render(request, 'home.html', {'widget_form': widget_form, 'widgets': widgets})
 
 
-class WidgetCreate(CreateView):
-    description = Widget
-    quantity = '__all__'
 
-class WidgetDelete(DeleteView):
-    description = Widget
-    quantity = '__all__'
+def add_widget(request):
+    	# create the ModelForm using the data in request.POST
+  form = WidgetForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_widget = form.save(commit=False)
+    new_widget.save()
+  return redirect('home')
+
+def delete_widget(request, widget_id):
+    w = Widget.objects.get(id=widget_id)
+# This will delete the Blog and all of its Entry objects.
+    w.delete()
+    	# create the ModelForm using the data in request.POST
+    return redirect('home')
